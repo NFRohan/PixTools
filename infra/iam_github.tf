f0a12803515487d2e505ccbc080641053bf39088
NFRohan <nfrohan110@gmail.com>
2026-02-27 03:34:18 +0600

    fix(deploy): formally manage GitHub Actions OIDC role in Terraform with exactly scoped permissions and add ALB polling loop to CD pipeline

---
+++ infra/iam_github.tf
@@ -0,0 +1,103 @@
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+resource "aws_iam_role" "github_actions_deploy" {
+  name = "GitHubActionsPixToolsDeployRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = data.aws_iam_openid_connect_provider.github.arn
+        }
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:NFRohan/PixTools:*"
+          }
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions_deploy" {
+  name = "pixtools-github-actions-deploy-policy"
+  role = aws_iam_role.github_actions_deploy.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "VisualEditor0"
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeInstances",
+          "ssm:DescribeInstanceInformation",
+          "ecr:GetAuthorizationToken",
+          "ssm:GetCommandInvocation",
+          "ssm:GetParameter",
+          "ssm:GetParameters",
+          "ssm:StartSession"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "VisualEditor1"
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:CompleteLayerUpload",
+          "ecr:DescribeImages",
+          "s3:ListBucket",
+          "ecr:UploadLayerPart",
+          "ecr:InitiateLayerUpload",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage"
+        ]
+        Resource = [
+          aws_ecr_repository.api.arn,
+          aws_ecr_repository.worker.arn,
+          aws_s3_bucket.manifests.arn
+        ]
+      },
+      {
+        Sid    = "VisualEditor2"
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "${aws_s3_bucket.manifests.arn}/*"
+      },
+      {
+        Sid    = "VisualEditor3"
+        Effect = "Allow"
+        Action = "ssm:SendCommand"
+        Resource = [
+          "arn:aws:ssm:${var.aws_region}::document/AWS-RunShellScript",
+          "arn:aws:ec2:${var.aws_region}:${data.aws_caller_identity.current.account_id}:instance/*"
+        ]
+      },
+      {
+        Sid    = "SsmDescribeInstances"
+        Effect = "Allow"
+        Action = [
+          "ssm:DescribeInstanceInformation"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+output "github_deploy_role_arn" {
+  value = aws_iam_role.github_actions_deploy.arn
+}
